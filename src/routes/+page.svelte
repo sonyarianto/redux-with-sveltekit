703bfb62c9f0f33233de8c6535e141c07d2a394c
Sony AK <sony@sony-ak.com>
2023-06-18 12:22:58 +0700

    minor changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,50 @@
 <script>
-  import { configureStore, createSlice } from '@reduxjs/toolkit';
-  import { onMount, afterUpdate } from 'svelte';
+	import { configureStore, createSlice } from '@reduxjs/toolkit';
+	import { onMount, afterUpdate } from 'svelte';
 
-  const counterSlice = createSlice({
-    name: 'counter',
-    initialState: { value: 0 },
-    reducers: {
-      increment: state => {
-        state.value += 1;
-      },
-      decrement: state => {
-        state.value -= 1;
-      },
-    },
-  });
+	const counterSlice = createSlice({
+		name: 'counter',
+		initialState: { value: 0 },
+		reducers: {
+			increment: (state) => {
+				state.value += 1;
+			},
+			decrement: (state) => {
+				state.value -= 1;
+			}
+		}
+	});
 
-  const store = configureStore({
-    reducer: {
-      counter: counterSlice.reducer,
-    },
-  });
+	const store = configureStore({
+		reducer: {
+			counter: counterSlice.reducer
+		}
+	});
 
-  let count = 0;
-  let unsubscribe;
+	let count = 0;
+	let unsubscribe;
 
-  onMount(() => {
-    // Subscribe to changes in the Redux store
-    
-    unsubscribe = store.subscribe(() => {
-      count = store.getState().counter.value;
-    });
+	onMount(() => {
+		// Subscribe to changes in the Redux store
 
-    return unsubscribe; // Unsubscribe from the store on component cleanup
-  });
+		unsubscribe = store.subscribe(() => {
+			count = store.getState().counter.value;
+		});
 
-  // Trigger reactivity within the component
+		return unsubscribe; // Unsubscribe from the store on component cleanup
+	});
 
-  afterUpdate(() => { // afterUpdate is called after the DOM is updated, onUpdate is called before afterUpdate
-    count; // seems no-op, but it triggers reactivity
-  });
+	// Trigger reactivity within the component
+
+	afterUpdate(() => {
+		// afterUpdate is called after the DOM is updated, onUpdate is called before afterUpdate
+		count; // seems no-op, but it triggers reactivity
+	});
 </script>
 
 <div>
-  <h1>Counter: {count}</h1>
-  
-  <button on:click={() => store.dispatch(counterSlice.actions.increment())}>Increment</button>
-  <button on:click={() => store.dispatch(counterSlice.actions.decrement())}>Decrement</button>
+	<h1>Counter: {count}</h1>
+
+	<button on:click={() => store.dispatch(counterSlice.actions.increment())}>Increment</button>
+	<button on:click={() => store.dispatch(counterSlice.actions.decrement())}>Decrement</button>
 </div>
